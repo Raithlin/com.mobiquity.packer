7ca37b86d315ac746257cc456800bcb4362a5794
Stephen Metcalfe <raithlin@gmail.com>
2022-01-27 00:37:15 +0200

    Minor refactoring

--- com.mobiquity.packer/Model/PackageItemModel.cs
+++ com.mobiquity.packer/Model/PackageItemModel.cs
@@ -13,17 +13,20 @@
             var objects = itemDetails.Split(',');
             if (objects.Length != 3) throw new APIException($"Invalid item format: {itemDetails}");
 
-            _ = int.TryParse(objects[0], out int index);
-            if (index == 0) throw new APIException($"Invalid index: {objects[0]}");            
-            Index = index;
+             if (!int.TryParse(objects[0], out int index))
+                throw new APIException($"Invalid index: {objects[0]}");
+             else
+                Index = index;
 
-            _ = double.TryParse(objects[1], out double weight);
-            if (weight == 0.0) throw new APIException($"Invalid weight: {objects[1]}");
-            Weight = weight;
+            if (!double.TryParse(objects[1], out double weight))
+                throw new APIException($"Invalid weight: {objects[1]}");
+            else
+                Weight = weight;
 
-            _ = double.TryParse(objects[2].Substring(1, objects[2].Length-1), out double cost);
-            if (cost == 0.0) throw new APIException($"Invalid cost: {objects[2]}");
-            Cost = cost;
+            if (!double.TryParse(objects[2][1..], out double cost))
+                throw new APIException($"Invalid cost: {objects[2]}");
+            else
+                Cost = cost;
         }
     }
 }
